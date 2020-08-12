5a49ef8928f1f4d3a634f4e8b077975fa132358c
glandty <glandtya@163.com>
2020-08-12 21:34:05 +0800

    Initial commit

---
+++ src/Demo.sql
@@ -0,0 +1,138 @@
+/*
+题目描述
+查找最晚入职员工的所有信息，为了减轻入门难度，目前所有的数据里员工入职的日期都不是同一天(sqlite里面的注释为--,mysql为comment)
+CREATE TABLE `employees` (
+`emp_no` int(11) NOT NULL,  -- '员工编号'
+`birth_date` date NOT NULL,
+`first_name` varchar(14) NOT NULL,
+`last_name` varchar(16) NOT NULL,
+`gender` char(1) NOT NULL,
+`hire_date` date NOT NULL,
+PRIMARY KEY (`emp_no`));
+ */
+ select * from employees where hire_date == (select max(hire_date) from employees)
+
+ /*
+ 题目描述
+查找入职员工时间排名倒数第三的员工所有信息，为了减轻入门难度，目前所有的数据里员工入职的日期都不是同一天
+CREATE TABLE `employees` (
+`emp_no` int(11) NOT NULL,
+`birth_date` date NOT NULL,
+`first_name` varchar(14) NOT NULL,
+`last_name` varchar(16) NOT NULL,
+`gender` char(1) NOT NULL,
+`hire_date` date NOT NULL,
+PRIMARY KEY (`emp_no`));
+  */
+  select * from employees order by hire_date desc limit 2,1
+
+  /*
+  题目描述
+查找各个部门当前(dept_manager.to_date='9999-01-01')领导当前(salaries.to_date='9999-01-01')薪水详情以及其对应部门编号dept_no
+(注:请以salaries表为主表进行查询，输出结果以salaries.emp_no升序排序，并且请注意输出结果里面dept_no列是最后一列)
+CREATE TABLE `salaries` (
+`emp_no` int(11) NOT NULL, -- '员工编号',
+`salary` int(11) NOT NULL,
+`from_date` date NOT NULL,
+`to_date` date NOT NULL,
+PRIMARY KEY (`emp_no`,`from_date`));
+CREATE TABLE `dept_manager` (
+`dept_no` char(4) NOT NULL, -- '部门编号'
+`emp_no` int(11) NOT NULL, --  '员工编号'
+`to_date` date NOT NULL,
+PRIMARY KEY (`emp_no`,`dept_no`));
+   */
+select s.* , d.dept_no from salaries s,dept_manager d
+where d.to_date='9999-01-01'
+and s.to_date ='9999-01-01'
+and s.emp_no = d.emp_no
+
+/*
+题目描述
+查找所有已经分配部门的员工的last_name和first_name以及dept_no(请注意输出描述里各个列的前后顺序)
+CREATE TABLE `dept_emp` (
+`emp_no` int(11) NOT NULL,
+`dept_no` char(4) NOT NULL,
+`from_date` date NOT NULL,
+`to_date` date NOT NULL,
+PRIMARY KEY (`emp_no`,`dept_no`));
+CREATE TABLE `employees` (
+`emp_no` int(11) NOT NULL,
+`birth_date` date NOT NULL,
+`first_name` varchar(14) NOT NULL,
+`last_name` varchar(16) NOT NULL,
+`gender` char(1) NOT NULL,
+`hire_date` date NOT NULL,
+PRIMARY KEY (`emp_no`));
+ */
+ select e.last_name,e.first_name,d.dept_no from employees e,dept_emp d where d.emp_no = e.emp_no
+
+ /*
+ 查找所有员工的last_name和first_name以及对应部门编号dept_no，也包括暂时没有分配具体部门的员工(请注意输出描述里各个列的前后顺序)
+CREATE TABLE `dept_emp` (
+`emp_no` int(11) NOT NULL,
+`dept_no` char(4) NOT NULL,
+`from_date` date NOT NULL,
+`to_date` date NOT NULL,
+PRIMARY KEY (`emp_no`,`dept_no`));
+CREATE TABLE `employees` (
+`emp_no` int(11) NOT NULL,
+`birth_date` date NOT NULL,
+`first_name` varchar(14) NOT NULL,
+`last_name` varchar(16) NOT NULL,
+`gender` char(1) NOT NULL,
+`hire_date` date NOT NULL,
+PRIMARY KEY (`emp_no`));
+  */
+  select e.last_name,e.first_name,d.dept_no from employees e left join dept_emp d on d.emp_no = e.emp_no
+
+  /*
+  题目描述
+查找所有员工入职时候的薪水情况，给出emp_no以及salary， 并按照emp_no进行逆序(请注意，一个员工可能有多次涨薪的情况)
+CREATE TABLE `employees` (
+`emp_no` int(11) NOT NULL,
+`birth_date` date NOT NULL,
+`first_name` varchar(14) NOT NULL,
+`last_name` varchar(16) NOT NULL,
+`gender` char(1) NOT NULL,
+`hire_date` date NOT NULL,
+PRIMARY KEY (`emp_no`));
+CREATE TABLE `salaries` (
+`emp_no` int(11) NOT NULL,
+`salary` int(11) NOT NULL,
+`from_date` date NOT NULL,
+`to_date` date NOT NULL,
+PRIMARY KEY (`emp_no`,`from_date`));
+   */
+select e.emp_no,s.salary
+from employees e,salaries s
+where e.emp_no = s.emp_no and e.hire_date = s.from_date
+order by e.emp_no desc
+
+/*
+题目描述
+查找薪水变动超过15次的员工号emp_no以及其对应的变动次数t
+CREATE TABLE `salaries` (
+`emp_no` int(11) NOT NULL,
+`salary` int(11) NOT NULL,
+`from_date` date NOT NULL,
+`to_date` date NOT NULL,
+PRIMARY KEY (`emp_no`,`from_date`));
+ */
+ select emp_no,count() t
+from salaries
+group by emp_no
+having t>15
+
+/*
+题目描述
+找出所有员工当前(to_date='9999-01-01')具体的薪水salary情况，对于相同的薪水只显示一次,并按照逆序显示
+CREATE TABLE `salaries` (
+`emp_no` int(11) NOT NULL,
+`salary` int(11) NOT NULL,
+`from_date` date NOT NULL,
+`to_date` date NOT NULL,
+PRIMARY KEY (`emp_no`,`from_date`));
+ */
+ select distinct salary from salaries where to_date='9999-01-01' order by salary desc
+
